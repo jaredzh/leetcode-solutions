b2ac753c2751bc1c4c48a41f9843e2c72fd6f73e
Jared Zhang <55931912+jaredzh@users.noreply.github.com>
2022-01-03 18:39:39 -0500

    Time: 16 ms (81.69%), Space: 13.4 MB (46.37%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [[0]*n for i in range(m)]
+        dp[0][0] = 1
+        for i in range(m):
+            for j in range(n):
+                if i > 0:
+                    dp[i][j] += dp[i-1][j]
+                if j > 0:
+                    dp[i][j] += dp[i][j-1]
+        return dp[-1][-1]
