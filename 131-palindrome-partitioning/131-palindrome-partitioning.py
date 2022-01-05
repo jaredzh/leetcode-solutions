6d1eac4e34ff42a83af1f8c9b78e373877f5ba22
Jared Zhang <55931912+jaredzh@users.noreply.github.com>
2022-01-04 23:03:44 -0500

    Time: 644 ms (75.05%), Space: 32.8 MB (25.34%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def partition(self, s):
+        """
+        :type s: str
+        :rtype: List[List[str]]
+        """
+        res = []
+        
+        def palin(s_in):
+            return s_in == s_in[::-1]
+        
+        def dfs(s, path):
+            if not s:
+                res.append(path[:])
+            for i in range(1, len(s)+1):
+                if palin(s[:i]):
+                    path.append(s[:i])
+                    dfs(s[i:], path)
+                    path.pop()
+           
+        dfs(s, [])
+        return res
+    
