7d249cb525c6eea824bb635950e76dd20a83281e
Jared Zhang <55931912+jaredzh@users.noreply.github.com>
2022-01-06 18:40:55 -0500

    Time: 118 ms (19.49%), Space: 13.6 MB (97.47%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -9,6 +9,6 @@ class Solution(object):
         for i, m in enumerate(mat):
             heapq.heappush(heap, [m.count(1), i])
         res = []
-        for i in range(k):
-            res.append(heapq.heappop(heap)[1])
-        return res
+        # for i in range(k):
+        #     res.append(heapq.heappop(heap)[1])
+        return [heapq.heappop(heap)[1] for i in range(k)]
