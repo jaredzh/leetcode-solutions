d61b68d8691d1004de7cffa269f509af98cea7dd
Jared Zhang <55931912+jaredzh@users.noreply.github.com>
2022-01-06 10:59:42 -0500

    Time: 40 ms (5.93%), Space: 13.4 MB (35.56%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.py
+++ 216-combination-sum-iii/216-combination-sum-iii.py
@@ -6,10 +6,10 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         res = []
-        def dfs(path, k_in, num):
-            if k_in == k and sum(path) == n:
+        def dfs(path, k_in, num, cur_sum):
+            if k_in == k and cur_sum == n:
                 res.append(path)
             for i in range(num, 10):
-                dfs(path+[i], k_in+1, i+1)
-        dfs([], 0, 1)
+                dfs(path+[i], k_in+1, i+1, cur_sum+i)
+        dfs([], 0, 1, 0)
         return res
