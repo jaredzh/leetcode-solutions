96ecb020eef88fc22d94dc96d654f57bb47bf3f9
Jared Zhang <55931912+jaredzh@users.noreply.github.com>
2022-01-07 14:50:59 -0500

    Time: 1377 ms (5.18%), Space: 25.6 MB (9.39%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
@@ -6,7 +6,4 @@ class Solution(object):
         """
         n = len(nums)
         hi, lo = nums.index(max(nums)), nums.index(min(nums))
-        k1, k2 = 1e9, 1e9
-        k1 = max(hi,lo)+1
-        k2 = n-min(hi,lo)
-        return min(k1, k2, min(hi, lo) + (n-max(hi, lo)+1))
+        return min(max(hi,lo)+1, n-min(hi,lo), min(hi, lo) + (n-max(hi, lo)+1))
