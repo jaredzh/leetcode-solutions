3935d0b40f1bab05f53b8017bba3ac50eb1ad7e7
Jared Zhang <55931912+jaredzh@users.noreply.github.com>
2022-01-22 21:29:18 -0500

    Time: 543 ms (58.62%), Space: 16.6 MB (62.07%) - LeetHub

--- 1054-distant-barcodes/1054-distant-barcodes.py
+++ 1054-distant-barcodes/1054-distant-barcodes.py
@@ -8,7 +8,6 @@ class Solution(object):
         n = len(barcodes)
         res = [0]*n
         l = [val for val, ct in d for _ in range(ct)]
-        print(l)
         for i in range(0, n, 2):
             res[i] = l.pop(0)
         for i in range(1, n, 2):
