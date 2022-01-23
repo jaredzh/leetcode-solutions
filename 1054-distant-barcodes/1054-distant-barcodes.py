0c6e7b1835ec5a2de39b8227dd6c41ab57567664
Jared Zhang <55931912+jaredzh@users.noreply.github.com>
2022-01-22 21:29:38 -0500

    Time: 416 ms (89.66%), Space: 16.8 MB (37.93%) - LeetHub

--- 1054-distant-barcodes/1054-distant-barcodes.py
+++ 1054-distant-barcodes/1054-distant-barcodes.py
@@ -4,12 +4,12 @@ class Solution(object):
         :type barcodes: List[int]
         :rtype: List[int]
         """
-        d = Counter(barcodes).most_common()
+        d = Counter(barcodes).most_common()[::-1]
         n = len(barcodes)
         res = [0]*n
         l = [val for val, ct in d for _ in range(ct)]
         for i in range(0, n, 2):
-            res[i] = l.pop(0)
+            res[i] = l.pop()
         for i in range(1, n, 2):
-            res[i] = l.pop(0)
+            res[i] = l.pop()
         return res
