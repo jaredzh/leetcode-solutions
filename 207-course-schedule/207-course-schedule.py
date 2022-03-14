0f332e6dbdaaf79f721609c86d2b181f17fed300
Jared Zhang <55931912+jaredzh@users.noreply.github.com>
2022-03-13 23:09:59 -0400

    Time: 68 ms (99.45%), Space: 14.8 MB (86.69%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        def topo_sort(n, G):
+            res, indegree = [], [0]*n
+            que = []
+            for i in range(n):
+                for j in G[i]:
+                    indegree[j] += 1
+
+            for i in range(n):
+                if indegree[i] == 0:
+                    que.append(i)
+
+            while que:
+                curr = que.pop(0)
+                res.append(curr)
+                for j in G[curr]:
+                    indegree[j] -= 1
+                    if indegree[j] == 0:
+                        que.append(j)
+            return res
+        
+        d = defaultdict(list)
+        for prereq in prerequisites:
+            d[prereq[0]].append(prereq[-1])
+        return len(topo_sort(numCourses, d)) == numCourses
