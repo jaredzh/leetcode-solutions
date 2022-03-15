a82be246386234982da2377c75a8d9005d1601c5
Jared Zhang <55931912+jaredzh@users.noreply.github.com>
2022-03-15 13:37:35 -0400

    Time: 862 ms (53.84%), Space: 30.9 MB (48.87%) - LeetHub

---
+++ 2195-append-k-integers-with-minimal-sum/2195-append-k-integers-with-minimal-sum.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def minimalKSum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        def f(n):
+            return n*(n+1)/2
+        res = 0
+        nums = [0]+nums
+        nums = list(set(nums))
+        nums.sort()
+        for i in range(len(nums)-1):
+            if not k:
+                break
+            n1, n2 = nums[i], nums[i+1]-1
+            to_add = n2-n1
+            if not to_add:
+                continue
+            if to_add <= k:
+                res += (f(n2)-f(n1))
+                k -= to_add
+            else:
+                res += (f(n1+k)-f(n1))
+                k = 0
+        if k:
+            res += (f(nums[-1]+k)-f(nums[-1]))
+        return res
