117d583abfa7f86f59eb20b02336cb11a6c153b7
Jared Zhang <55931912+jaredzh@users.noreply.github.com>
2022-03-16 11:14:59 -0400

    Time: 4102 ms (11.28%), Space: 24.9 MB (27.82%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.py
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.py
@@ -5,10 +5,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        nums2 = []
+        mapped = []
         for i, n in enumerate(nums):
             k = ""
             for s in str(n):
                 k += str(mapping[int(s)])
-            nums2.append((int(k), i))
-        return [x for _, x in sorted(zip(nums2, nums), key=lambda pair: pair[0])]
+            mapped.append((int(k), i))
+        return [x for _, x in sorted(zip(mapped, nums), key=lambda pair: pair[0])]
