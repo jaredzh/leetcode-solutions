2686cd983a8108d72c5d32a9ca95b43541e7b968
Jared Zhang <55931912+jaredzh@users.noreply.github.com>
2022-04-04 17:26:32 -0400

    Time: 1012 ms (73.86%), Space: 15 MB (83.56%) - LeetHub

--- 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.py
+++ 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.py
@@ -1,30 +1,15 @@
 class Solution:
     def numberOfWays(self, s: str) -> int:
-        pre_zeros = []
-        pre_ones = []
-        k1, k2 = 0, 0
+        d = {"0": 0, "01": 0, "010": 0, 
+             "1": 0, "10": 0, "101": 0}
+        
         for val in s:
-            pre_zeros.append(k1)
-            pre_ones.append(k2)
             if val == "0":
-                k1 += 1
+                d["0"] += 1
+                d["010"] += d["01"]
+                d["10"] += d["1"]
             else:
-                k2 += 1
-        
-        post_zeros, post_ones = [0]*len(s), [0]*len(s)
-        k1, k2 = 0, 0
-        for i in range(len(s)-1, -1, -1):
-            post_zeros[i] = k1
-            post_ones[i] = k2
-            if s[i] == "0":
-                k1 += 1
-            else:
-                k2 += 1
-        
-        res = 0
-        for i, val in enumerate(s):
-            if val == "0":
-                res += (pre_ones[i]*post_ones[i])
-            else:
-                res += (pre_zeros[i]*post_zeros[i])
-        return res
+                d["1"] += 1
+                d["101"] += d["10"]
+                d["01"] += d["0"]
+        return d["010"] + d["101"]
