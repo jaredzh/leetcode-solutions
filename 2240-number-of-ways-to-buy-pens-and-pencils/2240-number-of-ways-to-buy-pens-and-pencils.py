7e73ba1a0c6985c792c12663a4a9a4ab9561c10e
Jared Zhang <55931912+jaredzh@users.noreply.github.com>
2022-04-19 21:34:20 -0400

    Time: 1034 ms (16.26%), Space: 13.9 MB (58.61%) - LeetHub

--- 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.py
+++ 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.py
@@ -2,6 +2,5 @@ class Solution:
     def waysToBuyPensPencils(self, total: int, cost1: int, cost2: int) -> int:
         res = 0
         for i in range(total//cost1 + 1):
-            left = total - (cost1*i)
-            res += (left//cost2 + 1)
+            res += ((total - (cost1*i))//cost2 + 1)
         return res
