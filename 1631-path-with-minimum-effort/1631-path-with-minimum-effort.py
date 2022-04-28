56e9b5aa3dd239ee7fa53cbc94af49b245db99a2
Jared Zhang <55931912+jaredzh@users.noreply.github.com>
2022-04-27 23:02:55 -0400

    Time: 1064 ms (62.62%), Space: 15.3 MB (86.90%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,34 +1,25 @@
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        import numpy
-        left, right = 0, numpy.amax(heights) - numpy.amin(heights)
-        end = [len(heights), len(heights[0])]
-        def dfs(k, visited, i, j) -> bool:
-            if i == end[0]-1 and j == end[1]-1:
-                return True
-            visited[i][j] = True
-            cur_h = heights[i][j]
-            if i > 0 and not visited[i-1][j] and abs(cur_h-heights[i-1][j]) <= k:
-                if dfs(k, visited, i-1, j):
-                    return True
-            if i < end[0]-1 and not visited[i+1][j] and abs(cur_h-heights[i+1][j]) <= k:
-                if dfs(k, visited, i+1, j):
-                    return True
-            if j > 0 and not visited[i][j-1] and abs(cur_h-heights[i][j-1]) <= k:
-                if dfs(k, visited, i, j-1):
-                    return True
-            if j < end[1]-1 and not visited[i][j+1] and abs(cur_h-heights[i][j+1]) <= k:
-                if dfs(k, visited, i, j+1):
-                    return True
-            return False
+        m, n = len(heights), len(heights[0])
+        cost = [ [float("inf")] * n for _ in range(m) ]
+        DIR = [0, 1, 0, -1, 0]
+        pq = [(0, 0, 0)]
         
-        while left <= right:
-            mid = (left + right) // 2
-            vis = [ [False]*end[1] for _ in range(end[0]) ]
-            if not dfs(mid, vis, 0, 0):
-                left = mid + 1
-            else:
-                right = mid - 1
-        return left
+        while pq:
+            val, r, c = heapq.heappop(pq)
+            if cost[r][c] < val:
+                continue
+            if r == m-1 and c == n-1:
+                return val
+            for i in range(4):
+                row, col = r+DIR[i], c+DIR[i+1]
+                if 0 <= row < m and 0 <= col < n:
+                    k = abs(heights[row][col] - heights[r][c])
+                    new_cost = max(k, val)
+                    if new_cost < cost[row][col]:
+                        cost[row][col] = new_cost
+                        heapq.heappush(pq, (new_cost, row, col))
+            
+            
             
     
